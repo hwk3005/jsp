72606f9589fc1dcb4831b49564084300e1beb1d0
hwk3005 <hwk3005@naver.com>
2025-08-08 14:22:12 +0900

    new

---
+++ w0807/src/main/java/com/java/ex/FormPage2.java
@@ -0,0 +1,66 @@
+package com.java.ex;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/FormPage2")
+public class FormPage2 extends HttpServlet {
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doAction 실행");
+		String name = request.getParameter("name");
+		String kor = request.getParameter("kor");
+		String eng = request.getParameter("eng");
+		String math = request.getParameter("math");
+		int kor2 = Integer.parseInt(kor);
+		int eng2 = Integer.parseInt(eng);
+		int math2 = Integer.parseInt(math);
+		int total = kor2 + eng2 + math2;
+		double avg = total/3.0;
+		System.out.println("name: "+name);
+		System.out.println("kor: "+kor);
+		System.out.println("eng: "+eng);
+		System.out.println("math: "+math);
+		System.out.println("total: "+total);
+		System.out.println("avg: "+avg);
+
+		
+		// 웹페이지 생성 가능
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("<title>응답페이지</title>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h2>입력정보</h2>");
+		writer.println("<p>이름: "+name+"</p>");
+		writer.println("<p>국어: "+kor+"</p>");
+		writer.println("<p>영어: "+eng+"</p>");
+		writer.println("<p>수학: "+math+"</p>");
+		writer.println("<p>합계: "+total+"</p>");
+		writer.println("<p>평균: "+avg+"</p>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+		writer.close();
+		
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet 실행");
+		doAction(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost 실행");
+		doAction(request, response);
+	}
+
+}
